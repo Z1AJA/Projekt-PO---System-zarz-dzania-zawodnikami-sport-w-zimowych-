c67c83e71368703945a425880bdbb0fa8d9880d9
Z1AJA <5masterschalange@gmail.com>
2026-01-16 00:09:32 +0100

    Dodanie klasy snowbordzista

--- System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/Domain/Entities/Snowboardzista.cs
+++ System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/Domain/Entities/Snowboardzista.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using system_zawodnicy_zimowi.core.Domain.Enums;
 
 namespace system_zawodnicy_zimowi.core.Domain.Entities
 {
-    internal class Snowboardzista
+    public class Snowboardzista : Zawodnik
     {
+        public Snowboardzista(string imie, string nazwisko, int wiek) : base(imie, nazwisko, wiek, Dyscyplina.Snowboard) { }
+
+        public override object Clone()
+        {
+            var copy = new Snowboardzista(Imie, Nazwisko, Wiek);
+            copy.SetPunktyIRange(Punkty, Ranga);
+            return copy;
+        }
     }
 }
