b9a68e32080f614f315ba55014c4738198bc9217
Z1AJA <5masterschalange@gmail.com>
2026-01-16 15:30:10 +0100

    wypchniecie wszystkiego na gotowe

--- System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/obj/Debug/net8.0/system_zawodnicy_zimowi.core.AssemblyInfo.cs
+++ System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/obj/Debug/net8.0/system_zawodnicy_zimowi.core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a58040976b4e8b5c0537bb5a2dd6fc43a8e969a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225ce4a5edf29db2f4668aae58c5477d8f3a034d")]
 [assembly: System.Reflection.AssemblyProductAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
