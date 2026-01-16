2a58040976b4e8b5c0537bb5a2dd6fc43a8e969a
Z1AJA <5masterschalange@gmail.com>
2026-01-16 14:36:25 +0100

    On main: Dodanie interfejsuw repo dla data i gui

--- System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/obj/Debug/net8.0/system_zawodnicy_zimowi.core.AssemblyInfo.cs
+++ System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/obj/Debug/net8.0/system_zawodnicy_zimowi.core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd34afcfc1e0666db49c6a1ebcbdfe2c8c1a4fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c0183e497fac1d1c31a9a8fcbc481e12535dfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
