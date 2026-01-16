124f9f338d43c179101763aa491f005048ff6d5b
Z1AJA <5masterschalange@gmail.com>
2026-01-16 01:14:51 +0100

    dodano i uzupełniono logike eventow w serwisie PunktacjaService

--- System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/obj/Debug/net8.0/system_zawodnicy_zimowi.core.AssemblyInfo.cs
+++ System_zarzadzania_zawodnikami_sportow_zimowych/system_zawodnicy_zimowi.core/obj/Debug/net8.0/system_zawodnicy_zimowi.core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe6996e04af1edff7d2bf6305f446e2a9afd013")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a4b2b2c66080d70467aa02e5d492996921c1e65")]
 [assembly: System.Reflection.AssemblyProductAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("system_zawodnicy_zimowi.core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
